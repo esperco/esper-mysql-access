7066f60153f27d6563bc0cca262baa0514f52f16
Martin Jambon <martin@esper.co>
2013-10-18 12:40:40 -0700

    Missing file

---
+++ mysql_types.ml
@@ -0,0 +1,43 @@
+open Printf
+
+module type Serializable =
+sig
+  type t
+  val to_string : t -> string
+  val of_string : string -> t
+end
+
+module type Numeric =
+sig
+  type t
+  val to_float : t -> float
+  val of_float : float -> t
+end
+
+type direction = [ `Asc | `Desc ]
+    (* ascending or descending order *)
+
+(* used to write float data into the 'ord' columns *)
+let ord_of_float x =
+  match classify_float x with
+    FP_normal
+  | FP_subnormal
+  | FP_zero -> sprintf "%.17g" x (* enough digits to not lose precision *)
+  | FP_infinite
+  | FP_nan -> invalid_arg (sprintf "ord_of_float: %g" x)
+
+(* Commonly-used created/last-modified timestamps *)
+module KV = struct
+  let created k v = Util_time.now ()
+  let last_modified = Some (fun k v ord -> Util_time.now ())
+end
+
+module KKV = struct
+  let created k1 k2 v = Util_time.now ()
+  let last_modified = Some (fun k1 k2 v ord -> Util_time.now ())
+end
+
+module KK = struct
+  let created k1 k2 = Util_time.now ()
+  let last_modified = Some (fun k1 k2 ord -> Util_time.now ())
+end
